26da747071b7111c67edc6ee216808fa8b262910
james5635 <you@example.com>
2026-09-26 13:04:47 +0700

    update

--- 3. Queue/app/routes.ts
+++ 3. Queue/app/routes.ts
@@ -1,6 +1,7 @@
-import { type RouteConfig, index , route} from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx"),
-    route("ai-message-queue-processing", "routes/ai-message queue-processing.tsx")
+route("ai-message-queue-processing", "routes/ai-message queue-processing.tsx"),
+route("priority-based-download-manager", "routes/priority-based-download-manager.tsx")
 
 ] satisfies RouteConfig;
--- 3. Queue/app/routes/home.tsx
+++ 3. Queue/app/routes/home.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <>
     <a href="ai-message-queue-processing">ai-message-queue-processing</a>
+    <br />
+    <a href="priority-based-download-manager">priority-based-download-manager</a>
     </>
   )
 }
